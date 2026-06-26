d2ef4b33fa32af17b5b212452ebba3472a321577
Marat Safarov <164805788+1223383939393@users.noreply.github.com>
2026-06-26 09:56:45 +0300

    10 челлендж

---
+++ 10_challenge/10_challenge.py
@@ -0,0 +1,11 @@
+"""
+We will use this script to teach Python to absolute beginners
+The script is an example of Fizz-Buzz implemented in Python
+...
+"""
+
+import fizzbuzz
+
+#----START OF SCRIPT
+if __name__ == '__main__':
+    fizzbuzz.fizzbuzz(100)
