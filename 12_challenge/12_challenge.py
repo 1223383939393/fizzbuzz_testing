08af353ea7b366fc0fbc54bf53f52643d02dfdb4
Marat Safarov <164805788+1223383939393@users.noreply.github.com>
2026-06-26 09:57:18 +0300

    12 челлендж

---
+++ 12_challenge/12_challenge.py
@@ -0,0 +1,23 @@
+class FizzBuzz:
+
+    def __init__(self):
+        "Initializer"
+        self.num1 = 3
+        self.num2 = 5
+        self.three_mul = 'fizz'
+        self.five_mul = 'buzz'
+
+    def fizzbuzz(self, max_num):
+        "This method implements FizzBuzz"
+
+        for i in range(1, max_num):
+            if i % self.num1 == 0 and i % self.num2 == 0:
+                print(i, self.three_mul + self.five_mul)
+            elif i % self.num1 == 0:
+                print(i, self.three_mul)
+            elif i % self.num2 == 0:
+                print(i, self.five_mul)
+
+if __name__ == '__main__':
+    test_obj = FizzBuzz()
+    test_obj.fizzbuzz(100)
