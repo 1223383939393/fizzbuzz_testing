e263eecf8b995fd07e3596b14163694e958efeb2
Marat Safarov <164805788+1223383939393@users.noreply.github.com>
2026-06-26 09:57:03 +0300

    11 челлендж

---
+++ 11_challenge/11_challenge.py
@@ -0,0 +1,13 @@
+def fizzbuzz(max_num):
+    "This method implements FizzBuzz"
+    for i in range(1, max_num):
+        if i % 3 == 0 and i % 5 == 0:
+            print(i, "fizzbuzz")
+        elif i % 3 == 0:
+            print(i, "fizz")
+        elif i % 5 == 0:
+            print(i, "buzz")
+
+#----START OF SCRIPT
+if __name__ == '__main__':
+    fizzbuzz(100)
